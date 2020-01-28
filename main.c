321903f330f35b0ded371b25ad899e9c5e181d82
artbek <bart@artbek.co.uk>
2020-01-28 22:53:22 +0000

    Working version.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 1200000
 
@@ -12,15 +12,23 @@
 #define PIN_E PB4
 #define uchar unsigned char
 
+#define BB if (demo_stage_changed) { break; }
+
+uchar i;
 
 void clear_leds()
 {
-	PORTB &= ~_BV(PIN_A);
-	PORTB &= ~_BV(PIN_B);
-	PORTB &= ~_BV(PIN_C);
-	PORTB &= ~_BV(PIN_D);
+	PORTB &= 0b11110000;
 }
 
+#define COUNTER_SLEEP_DEFAULT 2000
+
+signed short int counter_sleep = COUNTER_SLEEP_DEFAULT;
+
+void reset_counter_sleep()
+{
+	counter_sleep = COUNTER_SLEEP_DEFAULT;
+}
 
 void go_to_sleep()
 {
@@ -32,9 +40,10 @@ void go_to_sleep()
 }
 
 
+unsigned char on_period;
 
-unsigned char demo_stage = 1;
-unsigned char max_demo_stage = 1;
+unsigned char demo_stage = 0;
+unsigned char max_demo_stage = 11;
 unsigned char demo_stage_changed = 0;
 	
 void rotate_demo_stage()
@@ -50,22 +59,10 @@ void rotate_demo_stage()
 
 char is_button1_armed = 0;
 
-void arm_button1()
-{
-	is_button1_armed = 1;
-}
-
-
-void unarm_button1()
-{
-	is_button1_armed = 0;
-}
-
 char is_button1_up()
 {
 	return (PINB & _BV(PIN_E));
 }
-
 
 char is_button1_down()
 {
@@ -77,42 +74,42 @@ void handle_button()
 {
 		if (! is_button1_armed) {
 			if (is_button1_down()) {
-				arm_button1();
-				_delay_ms(1);
+				is_button1_armed = 1;
+				_delay_us(100);
 			}
 		} else {
 			if (is_button1_up()) {
+				reset_counter_sleep();
 				rotate_demo_stage();
-				unarm_button1();
-				_delay_ms(1);
+				is_button1_armed = 0;
+				_delay_us(100);
 			}
 		}
 }
 
 
-void on(uchar a, uchar b, uchar c, uchar d, uchar period)
+void on(uchar leds_bitmask)
 {
 	clear_leds();
 
-	if (a) {
+	if (leds_bitmask & 0b1000) {
 		PORTB |= _BV(PIN_A);
 	}
 
-	if (b) {
+	if (leds_bitmask & 0b0100) {
 		PORTB |= _BV(PIN_B);
 	}
 
-	if (c) {
+	if (leds_bitmask & 0b0010) {
 		PORTB |= _BV(PIN_C);
 	}
 
-	if (d) {
+	if (leds_bitmask & 0b0001) {
 		PORTB |= _BV(PIN_D);
 	}
 
-	uchar i;
-	for (i = 0; i < period; i++) {
-		_delay_ms(1);
+	for (i = 0; i < on_period; i++) {
+		_delay_us(800);
 		handle_button();
 	}
 }
@@ -127,59 +124,151 @@ int __attribute__((noreturn)) main(void)
 
 
 	// diretions of pins
-	DDRB |= _BV(PIN_A);
-	DDRB |= _BV(PIN_B);
-	DDRB |= _BV(PIN_C);
-	DDRB |= _BV(PIN_D);
+	DDRB |= 0b00001111;
 
 	DDRB &= ~_BV(PIN_E); // input
 	PORTB |= _BV(PIN_E);
 
 
-	clear_leds();
+	cli();
+	TCCR0B |= (1 << CS00);
+	TIMSK0 |= (1 << TOIE0);
+	TCNT0 = 0;
+	sei();
 
-
-
-	signed long counter_sleep = 15000; // ~ 10 seconds
-
-		char speed = 4;
 
 	while (1) {
 
 		switch (demo_stage)
 		{
-			case 0:
-				speed = 4;
-				on(1, 0, 0, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 1, 0, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 0, 1, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 0, 0, 1, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 0, 1, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 1, 0, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				break;
+				case 0:
+					on_period = 5;
+					on(0b0010); BB;
+					on(0b0000); BB;
+					on(0b0100); BB;
+					on(0b0000); BB;
+					on(0b0001); BB;
+					on(0b0000); BB;
+					on(0b1000); BB;
+					on(0b0000); BB;
+					break;
 
-			case 1:
-				speed = 6;
-				on(1, 1, 1, 1, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				on(0, 0, 0, 0, speed);
-				if (demo_stage_changed) { demo_stage_changed = 0; break; }
-				break;
+				case 1:
+					on_period = 9;
+					on(0b0101); BB;
+					on(0b1010); BB;
+					break;
+
+				case 2:
+					on_period = 8;
+					on(0b1100); BB;
+					on(0b0011); BB;
+					break;
+
+				case 3:
+					on_period = 4;
+					on(0b1000); BB;
+					on(0b0100); BB;
+					on(0b0010); BB;
+					on(0b0001); BB;
+					on(0b0000); BB;
+					break;
+
+				case 4:
+					on_period = 3;
+					on(0b1000); BB;
+					on(0b1100); BB;
+					on(0b1110); BB;
+					on(0b1111); BB;
+					on(0b1111); BB;
+					on(0b0111); BB;
+					on(0b0011); BB;
+					on(0b0001); BB;
+					on(0b0000); BB;
+					break;
+
+				case 5:
+					on_period = 4;
+					on(0b1000); BB;
+					on(0b0100); BB;
+					on(0b0010); BB;
+					on(0b0001); BB;
+					on(0b0010); BB;
+					on(0b0100); BB;
+					break;
+
+				case 6:
+					on_period = 3;
+					on(0b1000); BB;
+					on(0b1100); BB;
+					on(0b1110); BB;
+					on(0b1111); BB;
+					on(0b1111); BB;
+					on(0b1110); BB;
+					on(0b1100); BB;
+					on(0b1000); BB;
+					on(0b0000); BB;
+					break;
+
+				case 7:
+					on_period = 8;
+					on(0b1111);
+					on(0b0000); BB;
+					break;
+
+				case 8:
+					on_period = 5;
+					on(0b1111);
+					on(0b0000); BB;
+					break;
+
+				case 9:
+					on_period = 4;
+					on(0b1111);
+					on(0b0000); BB;
+					break;
+
+				case 10:
+					on_period = 2;
+					on(0b1111);
+					on(0b0000); BB;
+					break;
+
+				case 11:
+					on_period = 1;
+					on(0b0111);
+					on(0b1011);
+					on(0b1101);
+					on(0b1110);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111);
+					on(0b1111); BB;
+					break;
+
 		}
 
+		demo_stage_changed = 0;
 
-		counter_sleep--;
+
 		if (counter_sleep < 0) {
-			//go_to_sleep();
+			go_to_sleep();
 		}
 
 
 	}
 
+}
+
+/**
+ *  * Called when TIMER0 overflows which is
+ *   * ca. F_CPU / 256 times a second (without timer prescaling).
+ *    */
+ISR(TIM0_OVF_vect)
+{
+	counter_sleep--;
 }
